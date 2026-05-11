1afb2b95f112b9f8f1907511397ba4a47e993fdd
tuanle <deadbush2004@gmail.com>
2026-05-11 11:41:10 +0700

    friend+search

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,37 @@
 import express from "express";
 import cors from "cors";
+
 import errorHandler from "./middlewares/error.middleware";
+
 import authRoute from "./modules/auth/auth.route";
 import userRoute from "./modules/users/user.route";
+
+import searchRoute from "./modules/search/search.route";
+import friendRoute from "./modules/friend/friend.route";
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 authRoute(app);
 userRoute(app);
+app.use("/api/search", searchRoute);
+app.use("/api/friend", friendRoute);
 
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend working"
+  });
+});
 
-app.use((req,res) => {
-    res.status(404).json({
-        success: false,
-        message: "Endpoint not found."
-    });
-})
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Endpoint not found."
+  });
+});
 
 app.use(errorHandler);
